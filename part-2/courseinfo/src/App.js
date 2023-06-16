6d8459d101311d26e3ad854a154e9ed630ccc9e4
disheng <ongdisheng@gmail.com>
2023-06-16 11:00:32 +0800

    Completed exercise 2.4

--- part-2/courseinfo/src/App.js
+++ part-2/courseinfo/src/App.js
@@ -1,7 +1,7 @@
 // header component
 const Header = (props) => (
   <>
-    <h1>{props.name}</h1>
+    <h2>{props.name}</h2>
   </>
 )
 
@@ -57,34 +57,61 @@ const Course = ({ course }) => {
 
 // app component
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course =>
+        <Course key={course.id} 
+                course={course} 
+        />
+      )}
+    </div>
+  )
 }
 
 export default App
