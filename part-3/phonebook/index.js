b2b06b54f675af67d37774efe9854ab474e49ace
disheng <ongdisheng@gmail.com>
2023-06-18 20:15:00 +0800

    Completed exercise 3.1

---
+++ part-3/phonebook/index.js
@@ -0,0 +1,40 @@
+// create server
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+// person data
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// handle get request
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+// listen to requests on specified port
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
