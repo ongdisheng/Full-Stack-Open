79fd589dae031ae65eef86a40c3df873520d0961
disheng <ongdisheng@gmail.com>
2023-06-20 21:16:51 +0800

    Completed exercise 3.13

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,7 +1,9 @@
 // create server
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 // middleware
@@ -10,33 +12,13 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 
-// person data
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 // handle get request for persons 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 
 // handle get request for info 
@@ -115,7 +97,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 // listen to requests on specified port
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part-3/phonebook/models/person.js
@@ -0,0 +1,33 @@
+// import library
+const mongoose = require('mongoose')
+
+// connect to database 
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+mongoose.set('strictQuery', false)
+mongoose
+    .connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+// define schema 
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+// format returned object from database
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+// export model
+module.exports = mongoose.model('Person', personSchema)
