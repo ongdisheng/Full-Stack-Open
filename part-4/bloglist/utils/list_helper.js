5e3532f72deb1b53e5cd4e1a2474d8d0318c8920
disheng <ongdisheng@gmail.com>
2023-06-22 18:36:02 +0800

    Completed exercise 4.5

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -1,12 +1,28 @@
+// dummy function
 const dummy = (blogs) => {
     return 1
 } 
 
+// function used to calculate total number of likes
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+// function used to retrieve blog with most likes 
+const favoriteBlog = (blogs) => {
+    const reducer = (prev, current) => {
+        return prev.likes > current.likes 
+            ? prev
+            : current 
+    }
+
+    return blogs.length === 0
+        ? null
+        : blogs.reduce(reducer)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
