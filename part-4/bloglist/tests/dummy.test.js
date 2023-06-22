57d7001a7a1b25ac0b398802bbef536cf85cb1f9
disheng <ongdisheng@gmail.com>
2023-06-22 16:52:34 +0800

    Completed exercise 4.3

---
+++ part-4/bloglist/tests/dummy.test.js
@@ -0,0 +1,9 @@
+// import helper function
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
