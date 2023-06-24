992a3a52c4f23f54acd7035dd561b6b3adb73043
disheng <ongdisheng@gmail.com>
2023-06-24 11:57:42 +0800

    Completed exercise 4.9

--- part-4/bloglist/tests/blog_api.test.js
+++ part-4/bloglist/tests/blog_api.test.js
@@ -1,19 +1,40 @@
 // import statements
 const supertest = require('supertest')
 const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+// reset database state before running each test
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
 })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(0)
+describe('HTTP GET', () => {
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+    
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })    
+
+    test('unique identifier is id', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+        expect(response.body[0]._id).not.toBeDefined()
+    })
 })
 
 // run after all tests finish execution
