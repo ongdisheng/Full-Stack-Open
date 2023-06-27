6b9c363ac778be0d9b470e74ad4ae9d322b09b4f
disheng <ongdisheng@gmail.com>
2023-06-27 10:21:57 +0800

    Completed exercise 5.2

--- part-5/bloglist-frontend/src/App.js
+++ part-5/bloglist-frontend/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  // retrieve blogs from server
   useEffect(() => {
     blogService
       .getAll()
@@ -19,14 +20,30 @@ const App = () => {
       })
   }, [])
 
+  // 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   // event handler for login
   const handleLogin = async event => {
     event.preventDefault()
 
     const user = await loginService.login({ username, password })
+    window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
     setUser(user)
     setUsername('')
     setPassword('')
+  }
+
+  // event handler for logout
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   // render login form
@@ -63,6 +80,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       {user.name} logged in 
+      <button onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
