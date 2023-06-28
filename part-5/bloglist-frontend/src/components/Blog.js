404aa10f03ed7a4ab5afa21513c0e6996cbac424
disheng <ongdisheng@gmail.com>
2023-06-28 20:29:53 +0800

    Completed exercise 5.7

--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -29,7 +29,7 @@ const Blog = ({ blog }) => {
           <div>
             <div>{blog.url}</div>
             <div>likes {blog.likes} <button>like</button></div>
-            <div>{blog.author}</div>
+            <div>{blog.user.name}</div>
           </div>
         }
       </div>
