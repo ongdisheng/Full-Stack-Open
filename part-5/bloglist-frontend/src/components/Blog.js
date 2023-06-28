544ba1dfa76ba3e551d6763fa05aae0a6bd55a0b
disheng <ongdisheng@gmail.com>
2023-06-28 20:02:39 +0800

    Completed exercise 5.7

--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+// import library
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detail, setDetail] = useState(false)
+
+  // event handler for button
+  const toggleDetail = () => {
+    setDetail(!detail)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleDetail}>
+          { detail ? 'hide' : 'view' }
+        </button>
+        {
+          detail &&
+          <div>
+            <div>{blog.url}</div>
+            <div>likes {blog.likes} <button>like</button></div>
+            <div>{blog.author}</div>
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
 
 export default Blog
