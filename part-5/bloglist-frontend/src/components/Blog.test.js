0ef00a374b592834b4e66deef001e220027d7c90
disheng <ongdisheng@gmail.com>
2023-07-06 14:28:36 +0800

    Completed exercise 5.14

--- part-5/bloglist-frontend/src/components/Blog.test.js
+++ part-5/bloglist-frontend/src/components/Blog.test.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('render title and author only by default', () => {
+let blog = null
+beforeEach(() => {
   // define hardcoded blog
-  const blog = {
+  blog = {
     title: 'Blockchain Developer Roadmap',
     author: 'Mary Jane',
     url: 'https://roadmap.sh/blockchain',
-    likes: 60
+    likes: 60,
+    user: {
+      username: 'harry',
+      name: 'Harry Kane'
+    }
   }
+})
 
+test('render title and author only by default', () => {
   const { container } = render(<Blog blog={blog} />)
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent('Blockchain Developer Roadmap')
   expect(div).toHaveTextContent('Mary Jane')
   expect(div).not.toHaveTextContent('https://roadmap.sh/blockchain')
   expect(div).not.toHaveTextContent('60')
+})
+
+test('render url and likes after button click', async () => {
+  // setup user event
+  const user = userEvent.setup()
+
+  // simulate button click
+  const { container } = render(<Blog blog={blog} user={user} />)
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent('https://roadmap.sh/blockchain')
+  expect(div).toHaveTextContent('60')
 })
