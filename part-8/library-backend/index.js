d0af60713f9266cd992fc8f88b57c126a81487e9
disheng <ongdisheng@gmail.com>
2023-07-18 15:54:40 +0800

    Completed exercise 8.6

--- part-8/library-backend/index.js
+++ part-8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -122,6 +123,15 @@ const typeDefs = `
     ): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -149,6 +159,20 @@ const resolvers = {
       return result
     },
     allAuthors: () => authors
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = { ...args  }
+      books = books.concat(newBook)
+
+      // save new author
+      if (!authors.find(author => author.name === args.author)) {
+        const newAuthor = { name: args.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      return newBook
+    }
   },
   Author: {
     bookCount: (root) => {
